f21120d2ece87e44c58a265fcd625cc003815635
DanielLeThug <goldennoob68@gmail.com>
2017-10-22 14:18:05 +0200

    Popper
    Ajout de popper

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <script src="js/jquery-3.2.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
 </head>
 <body>
