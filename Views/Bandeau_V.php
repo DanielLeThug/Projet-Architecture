15cf399295a1b19f52180fce2fe4e330b9e2a8a6
Kazouke <kazouke@hotmail.fr>
2017-12-02 20:37:43 +0100

    Auto stash before merge of "master" and "origin/master"

--- Views/Bandeau_V.php
+++ Views/Bandeau_V.php
@@ -4,32 +4,33 @@
     <title>Projet Architecture</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<!-- Optional theme -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 </head>
 <body>
-	<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-		<div class="navbar-header">
+	<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0;" >
+		<div class="container-fluid">
 		<ul class="nav navbar-nav">
 			<li> <a href="index.php">Accueil</a> </li>
 			<li> <a href="login">Connexion</a> </li>
-			<li> <a href="add_user">S'enregistrer</a> </li>
-			<li> <a href="logout">Se déconnecter</a> </li>
-			<li>Menu des cours</li>
-			<li class="dropdown">
-				<ul class="dropdown-menu">
+			<li> <a href="add_user">S'enregistrer</a> </li> 
+			<div class="btn-group">
+				<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Menu des cours
+				<span class="caret"></span></button>
+				
+				<ul class="dropdown-menu" role="menu">
 					<li><a href="add_lesson">Ajouter un cours</a></li>
-				</ul>
-				<ul class="dropdown-menu">
 					<li><a href="view_lesson">Voir les cours</a></li>
 				</ul>
-			</li>
+
+			</div>
+			<li> <a href="logout">Se déconnecter</a> </li>
 		</ul>
 		</div>
+	  <!-- Navbar content -->
 	</nav>
+
 </body>
 </html>
