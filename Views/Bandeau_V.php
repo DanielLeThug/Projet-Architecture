bbb00eb61f5b7cc6bf726adf006c645a9aacfa44
Kazouke <kazouke@hotmail.fr>
2017-12-02 21:01:54 +0100

    Indenté

--- Views/Bandeau_V.php
+++ Views/Bandeau_V.php
@@ -9,35 +9,37 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 </head>
-<body>
-	<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0;" >
-        <div class="container-fluid">
-		<ul class="nav navbar-nav">
-		<li><form class="navbar-form navbar-right inline-form">
-			<ul class="nav navbar-nav">
-					<li> <a href="index.php">Accueil</a> </li>
-					<li> <div class="btn-group">
-						<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Menu des cours
-						<span class="caret"></span></button>
+	<body>
+		<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0;" >
+			<div class="container-fluid">
+				<ul class="nav navbar-nav">
+					<li>
+						<form class="navbar-form navbar-right inline-form">
+							<ul class="nav navbar-nav">
+									<li><a href="index.php">Accueil</a></li>
+									<li><div class="btn-group">
+										<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Menu des cours<span class="caret"></span></button>
+										<ul class="dropdown-menu" role="menu">
+											<li><a href="add_lesson">Ajouter un cours</a></li>
+											<li><a href="view_lesson">Voir les cours</a></li>
+										</ul>
+									</div></li>
+							</ul>
+						</form>
+					</li>
+					<li>
+						<form class="navbar-form navbar-right inline-form">
+							<ul class="nav navbar-nav">
+								<li> <a href="login">Connexion</a> </li>
+								<li> <a href="add_user">S'enregistrer</a> </li> 
+								<li> <a href="logout">Se déconnecter</a> </li>
+							</ul>
+						</form>
+					</li>
+				</ul>
+			</div>
+		  <!-- Navbar content -->
+		</nav>
 
-						<ul class="dropdown-menu" role="menu">
-							<li><a href="add_lesson">Ajouter un cours</a></li>
-							<li><a href="view_lesson">Voir les cours</a></li>
-						</ul>
-					</div></li>
-			</ul>
-		</form></li>
-        <li><form class="navbar-form navbar-right inline-form">
-            <ul class="nav navbar-nav">
-                <li> <a href="login">Connexion</a> </li>
-                <li> <a href="add_user">S'enregistrer</a> </li> 
-                <li> <a href="logout">Se déconnecter</a> </li>
-            </ul>
-        </form></li>
-		</ul>
-        </div>
-      <!-- Navbar content -->
-    </nav>
-
-</body>
+	</body>
 </html>
