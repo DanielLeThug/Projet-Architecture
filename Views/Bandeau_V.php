2a8f88ded8926f47e6ca20465dbf16c389d5cd94
Chackal <link684@msn.com>
2017-12-02 21:05:58 +0100

    indentation ma bite

--- Views/Bandeau_V.php
+++ Views/Bandeau_V.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <title>Projet Architecture</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<head>
+		<title>Projet Architecture</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-</head>
+	</head>
 	<body>
 		<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0;" >
 			<div class="container-fluid">
@@ -16,14 +16,14 @@
 					<li>
 						<form class="navbar-form navbar-right inline-form">
 							<ul class="nav navbar-nav">
-									<li><a href="index.php">Accueil</a></li>
-									<li><div class="btn-group">
-										<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Menu des cours<span class="caret"></span></button>
-										<ul class="dropdown-menu" role="menu">
-											<li><a href="add_lesson">Ajouter un cours</a></li>
-											<li><a href="view_lesson">Voir les cours</a></li>
-										</ul>
-									</div></li>
+								<li><a href="index.php">Accueil</a></li>
+								<li><div class="btn-group">
+									<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Menu des cours<span class="caret"></span></button>
+									<ul class="dropdown-menu" role="menu">
+										<li><a href="add_lesson">Ajouter un cours</a></li>
+										<li><a href="view_lesson">Voir les cours</a></li>
+									</ul>
+								</div></li>
 							</ul>
 						</form>
 					</li>
