153e820bb3bd9a636e8f01f16aa573269ad8b145
Kazouke <kazouke@hotmail.fr>
2018-01-03 17:09:44 +0100

    Correction bug BDD
    Les créations de tables sont dans l'ordre

--- uha_archiweb2017_g01sp02.sql
+++ uha_archiweb2017_g01sp02.sql
@@ -13,8 +13,6 @@ File Encoding         : 65001
 Date: 2018-01-02 17:37:54
 */
 
-SET FOREIGN_KEY_CHECKS=0;
-
 -- ----------------------------
 -- Table structure for batiments
 -- ----------------------------
@@ -27,69 +25,6 @@ CREATE TABLE `batiments` (
 
 -- ----------------------------
 -- Records of batiments
--- ----------------------------
-
--- ----------------------------
--- Table structure for concerne
--- ----------------------------
-DROP TABLE IF EXISTS `concerne`;
-CREATE TABLE `concerne` (
-  `ID_FORMATION` int(32) NOT NULL,
-  `ID_MATIERE` int(32) NOT NULL,
-  PRIMARY KEY (`ID_FORMATION`,`ID_MATIERE`),
-  KEY `ID_MATIERE_CONCERNE` (`ID_MATIERE`),
-  CONSTRAINT `ID_FORMATION_CONCERNE` FOREIGN KEY (`ID_FORMATION`) REFERENCES `formations` (`ID`),
-  CONSTRAINT `ID_MATIERE_CONCERNE` FOREIGN KEY (`ID_MATIERE`) REFERENCES `matieres` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of concerne
--- ----------------------------
-
--- ----------------------------
--- Table structure for cookies
--- ----------------------------
-DROP TABLE IF EXISTS `cookies`;
-CREATE TABLE `cookies` (
-  `id` int(32) NOT NULL,
-  `token` char(64) NOT NULL,
-  `user_id` int(32) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`) USING BTREE,
-  UNIQUE KEY `token` (`token`) USING BTREE,
-  UNIQUE KEY `cookies_ibfk_1` (`user_id`) USING BTREE,
-  CONSTRAINT `cookies_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `utilisateurs` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of cookies
--- ----------------------------
-
--- ----------------------------
--- Table structure for cours
--- ----------------------------
-DROP TABLE IF EXISTS `cours`;
-CREATE TABLE `cours` (
-  `ID_MATIERE` int(32) NOT NULL,
-  `ID_DATE` int(32) NOT NULL,
-  `ID_SALLE` int(32) NOT NULL,
-  `ID_VACATAIRE` int(32) NOT NULL,
-  `ID_VIREMENT` int(32) DEFAULT NULL,
-  `EFFECTUE` tinyint(4) NOT NULL,
-  PRIMARY KEY (`ID_MATIERE`,`ID_DATE`,`ID_SALLE`,`ID_VACATAIRE`),
-  KEY `ID_DATE_COURS` (`ID_DATE`),
-  KEY `ID_VIREMENT_COURS` (`ID_VIREMENT`),
-  KEY `ID_SALLE_COURS` (`ID_SALLE`),
-  KEY `ID_VACATAIRE_COURS` (`ID_VACATAIRE`),
-  CONSTRAINT `ID_DATE_COURS` FOREIGN KEY (`ID_DATE`) REFERENCES `dates_de_cours` (`ID`),
-  CONSTRAINT `ID_MATIERE_COURS` FOREIGN KEY (`ID_MATIERE`) REFERENCES `matieres` (`ID`),
-  CONSTRAINT `ID_SALLE_COURS` FOREIGN KEY (`ID_SALLE`) REFERENCES `salles` (`ID`),
-  CONSTRAINT `ID_VACATAIRE_COURS` FOREIGN KEY (`ID_VACATAIRE`) REFERENCES `utilisateurs` (`ID`),
-  CONSTRAINT `ID_VIREMENT_COURS` FOREIGN KEY (`ID_VIREMENT`) REFERENCES `virements` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of cours
 -- ----------------------------
 
 -- ----------------------------
@@ -108,24 +43,6 @@ CREATE TABLE `dates_de_cours` (
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for documents_administratifs
--- ----------------------------
-DROP TABLE IF EXISTS `documents_administratifs`;
-CREATE TABLE `documents_administratifs` (
-  `ID` int(32) NOT NULL AUTO_INCREMENT,
-  `LIBELLÉ` char(32) DEFAULT NULL,
-  `ID_VACATAIRE` int(32) DEFAULT NULL,
-  `ID_RESPONSABLE_ADMINISTRATIF` int(32) DEFAULT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `ID_RESPONSABLE_ADMINISTRATIF_DOC` (`ID_RESPONSABLE_ADMINISTRATIF`),
-  CONSTRAINT `ID_RESPONSABLE_ADMINISTRATIF_DOC` FOREIGN KEY (`ID_RESPONSABLE_ADMINISTRATIF`) REFERENCES `utilisateurs` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of documents_administratifs
--- ----------------------------
-
--- ----------------------------
 -- Table structure for formations
 -- ----------------------------
 DROP TABLE IF EXISTS `formations`;
@@ -137,23 +54,6 @@ CREATE TABLE `formations` (
 
 -- ----------------------------
 -- Records of formations
--- ----------------------------
-
--- ----------------------------
--- Table structure for matieres
--- ----------------------------
-DROP TABLE IF EXISTS `matieres`;
-CREATE TABLE `matieres` (
-  `ID` int(32) NOT NULL AUTO_INCREMENT,
-  `LIBELLÉ` char(32) DEFAULT NULL,
-  `ID_TYPE_DE_COURS` int(32) DEFAULT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `ID_TYPE_DE_COURS_MATIERE` (`ID_TYPE_DE_COURS`),
-  CONSTRAINT `ID_TYPE_DE_COURS_MATIERE` FOREIGN KEY (`ID_TYPE_DE_COURS`) REFERENCES `types_de_cours` (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of matieres
 -- ----------------------------
 
 -- ----------------------------
@@ -189,11 +89,28 @@ CREATE TABLE `types_de_cours` (
 -- ----------------------------
 
 -- ----------------------------
+-- Table structure for matieres
+-- ----------------------------
+DROP TABLE IF EXISTS `matieres`;
+CREATE TABLE `matieres` (
+  `ID` int(32) NOT NULL AUTO_INCREMENT,
+  `LIBELLÉ` char(32) DEFAULT NULL,
+  `ID_TYPE_DE_COURS` int(32) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `ID_TYPE_DE_COURS_MATIERE` (`ID_TYPE_DE_COURS`),
+  CONSTRAINT `ID_TYPE_DE_COURS_MATIERE` FOREIGN KEY (`ID_TYPE_DE_COURS`) REFERENCES `types_de_cours` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of matieres
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for utilisateurs
 -- ----------------------------
 DROP TABLE IF EXISTS `utilisateurs`;
 CREATE TABLE `utilisateurs` (
-  `ID` int(32) NOT NULL,
+  `ID` int(32) NOT NULL AUTO_INCREMENT,
   `PROFIL` int(32) DEFAULT NULL,
   `NOM` varchar(255) DEFAULT NULL,
   `PRENOM` varchar(255) DEFAULT NULL,
@@ -208,11 +125,48 @@ CREATE TABLE `utilisateurs` (
 INSERT INTO `utilisateurs` VALUES ('6', '1', 'Gates', 'Bill', 'admin@root.fr', '$2y$10$UIlO5RuFrmIXazBRD4aDheRl68dwkZ9cLaFXFF9XkJd/SGdX2Vt6m');
 
 -- ----------------------------
+-- Table structure for documents_administratifs
+-- ----------------------------
+DROP TABLE IF EXISTS `documents_administratifs`;
+CREATE TABLE `documents_administratifs` (
+  `ID` int(32) NOT NULL AUTO_INCREMENT,
+  `LIBELLÉ` char(32) DEFAULT NULL,
+  `ID_VACATAIRE` int(32) DEFAULT NULL,
+  `ID_RESPONSABLE_ADMINISTRATIF` int(32) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `ID_RESPONSABLE_ADMINISTRATIF_DOC` (`ID_RESPONSABLE_ADMINISTRATIF`),
+  CONSTRAINT `ID_RESPONSABLE_ADMINISTRATIF_DOC` FOREIGN KEY (`ID_RESPONSABLE_ADMINISTRATIF`) REFERENCES `utilisateurs` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of documents_administratifs
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for cookies
+-- ----------------------------
+DROP TABLE IF EXISTS `cookies`;
+CREATE TABLE `cookies` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `token` char(64) NOT NULL,
+  `user_id` int(32) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`) USING BTREE,
+  UNIQUE KEY `token` (`token`) USING BTREE,
+  UNIQUE KEY `cookies_ibfk_1` (`user_id`) USING BTREE,
+  CONSTRAINT `cookies_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `utilisateurs` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of cookies
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for virements
 -- ----------------------------
 DROP TABLE IF EXISTS `virements`;
 CREATE TABLE `virements` (
-  `ID` int(32) NOT NULL,
+  `ID` int(32) NOT NULL AUTO_INCREMENT,
   `DATE_V` char(32) DEFAULT NULL,
   `ID_RESPONSABLE_FINANCIER` int(32) DEFAULT NULL,
   PRIMARY KEY (`ID`),
@@ -223,4 +177,47 @@ CREATE TABLE `virements` (
 -- ----------------------------
 -- Records of virements
 -- ----------------------------
-SET FOREIGN_KEY_CHECKS=1;
+
+-- ----------------------------
+-- Table structure for cours
+-- ----------------------------
+DROP TABLE IF EXISTS `cours`;
+CREATE TABLE `cours` (
+  `ID_MATIERE` int(32) NOT NULL,
+  `ID_DATE` int(32) NOT NULL,
+  `ID_SALLE` int(32) NOT NULL,
+  `ID_VACATAIRE` int(32) NOT NULL,
+  `ID_VIREMENT` int(32) DEFAULT NULL,
+  `EFFECTUE` tinyint(4) NOT NULL,
+  PRIMARY KEY (`ID_MATIERE`,`ID_DATE`,`ID_SALLE`,`ID_VACATAIRE`),
+  KEY `ID_DATE_COURS` (`ID_DATE`),
+  KEY `ID_VIREMENT_COURS` (`ID_VIREMENT`),
+  KEY `ID_SALLE_COURS` (`ID_SALLE`),
+  KEY `ID_VACATAIRE_COURS` (`ID_VACATAIRE`),
+  CONSTRAINT `ID_DATE_COURS` FOREIGN KEY (`ID_DATE`) REFERENCES `dates_de_cours` (`ID`),
+  CONSTRAINT `ID_MATIERE_COURS` FOREIGN KEY (`ID_MATIERE`) REFERENCES `matieres` (`ID`),
+  CONSTRAINT `ID_SALLE_COURS` FOREIGN KEY (`ID_SALLE`) REFERENCES `salles` (`ID`),
+  CONSTRAINT `ID_VACATAIRE_COURS` FOREIGN KEY (`ID_VACATAIRE`) REFERENCES `utilisateurs` (`ID`),
+  CONSTRAINT `ID_VIREMENT_COURS` FOREIGN KEY (`ID_VIREMENT`) REFERENCES `virements` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of cours
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for concerne
+-- ----------------------------
+DROP TABLE IF EXISTS `concerne`;
+CREATE TABLE `concerne` (
+  `ID_FORMATION` int(32) NOT NULL,
+  `ID_MATIERE` int(32) NOT NULL,
+  PRIMARY KEY (`ID_FORMATION`,`ID_MATIERE`),
+  KEY `ID_MATIERE_CONCERNE` (`ID_MATIERE`),
+  CONSTRAINT `ID_FORMATION_CONCERNE` FOREIGN KEY (`ID_FORMATION`) REFERENCES `formations` (`ID`),
+  CONSTRAINT `ID_MATIERE_CONCERNE` FOREIGN KEY (`ID_MATIERE`) REFERENCES `matieres` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of concerne
+-- ----------------------------
