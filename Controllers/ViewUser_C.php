de9a381592ecf21917e840ae4854d9df03d2374d
Chackal <link684@msn.com>
2018-01-03 21:12:14 +0100

    Amélioration de la vue de la liste des users

--- Controllers/ViewUser_C.php
+++ Controllers/ViewUser_C.php
@@ -5,9 +5,35 @@ class ViewUser_C extends Controller {
 	public static function onLoad()
 	{
 		$users = ViewUser_M::listUser();
+		$i = 0;
 		foreach($users as $user)
 		{
-			echo "<tr><td>" . $user[0] . "</td><td>" . $user[1] . "</td><td>" . $user[2] . "</td><td>" . $user[3] . "</td><td>" . $user[4] . "</td></tr>";
+			$i++;
+			echo "<tr><td>" . $i . "</td><td>";
+			switch($user[1])
+			{
+				case 1:
+					$profil = "Administrateur";
+					break;
+				case 2:
+					$profil = "Contrôle de gestion";
+					break;
+				case 3:
+					$profil = "Responsable de Formation";
+					break;
+				case 4:
+					$profil = "Responsable Administratif de formation";
+					break;
+				case 5:
+					$profil = "Responsable Financier";
+					break;
+				case 6:
+					$profil = "Vacataire";
+					break;
+				default:
+					$profil = "";
+			}
+			echo "" . $profil . "</td><td>" . $user[2] . "</td><td>" . $user[3] . "</td><td>" . $user[4] . "</td><td>" . $user[5] . "</td></tr>";
 		}
         require_once("./Views/ViewUser2_V.php");
 	}
