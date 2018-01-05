40e698b867d3bdf0d32085c3fce3403aabb6bcea
Chackal <link684@msn.com>
2018-01-05 20:55:45 +0100

    pouet2

--- Views/ViewLesson_V.php
+++ Views/ViewLesson_V.php
@@ -8,10 +8,10 @@
 					<th scope="col">Professeur</th>
 					<th scope="col">Salle</th>
 					<th scope="col">Virement Effectué</th>
-			<th scope="col"></th>
-			<th scope="col"></th>
-		</tr>
-	</thead>
+					<th scope="col"></th>
+					<th scope="col"></th>
+				</tr>
+			</thead>
 			<tbody>
 				<?php
 					$lessons = ViewLesson_M::listLesson();
@@ -27,15 +27,16 @@
 						{
 							$virement = 'Non | <a href="validate_payment&id_cours=' . $lesson['id_cours'] . '" >Valider paiement <span class="glyphicon glyphicon-euro"></span></a> ';
 						}
-			echo "<tr><td>" . $i;
-			echo "</td><td>" . $lesson['Intitulé'] . " " . $lesson['Type'];
-			echo "</td><td>" . $lesson['Date'];
-			echo "</td><td>" . $lesson['Professeur'];
-			echo "</td><td>" . $lesson['Salle'];
-			echo "</td><td>" . $virement;
-			echo "</td><td><a href='edit_lesson&id=" . $lesson['Id'] . "'><span class='glyphicon glyphicon-pencil'></span></a>";
-			echo "</td><td><a href='view_lesson&id=" . $lesson['Id'] . "'><span class='glyphicon glyphicon-remove'></span></a></td></tr>";
-		}
-		?>
-	</tbody>
-</div>
+					echo "<tr><td>" . $i;
+					echo "</td><td>" . $lesson['Intitulé'] . " " . $lesson['Type'];
+					echo "</td><td>" . $lesson['Date'];
+					echo "</td><td>" . $lesson['Professeur'];
+					echo "</td><td>" . $lesson['Salle'];
+					echo "</td><td>" . $virement;
+					echo "</td><td><a href='edit_lesson&id=" . $lesson['Id'] . "'><span class='glyphicon glyphicon-pencil'></span></a>";
+					echo "</td><td><a href='view_lesson&id=" . $lesson['Id'] . "'><span class='glyphicon glyphicon-remove'></span></a></td></tr>";
+					}
+				?>
+			</tbody>
+		</table>
+	</div>
