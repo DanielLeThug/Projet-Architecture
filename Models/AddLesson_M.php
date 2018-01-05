8647ce0770492704edbe0a9e0f97743fe29f4dff
Kazouke <kazouke@hotmail.fr>
2018-01-05 17:30:36 +0100

    Modèle ajout cours
    Petite fonction addLessons des familles

--- Models/AddLesson_M.php
+++ Models/AddLesson_M.php
@@ -18,11 +18,12 @@ class AddLesson_M extends Model {
 							INNER JOIN batiments ON salles.ID_BATIMENT = batiments.ID');
 	}
 	
-	public static function addLesson($matiere, $date, $heure , $professeur, $salle)
-	{
-        self::query('INSERT INTO dates_de_cours VALUES (NULL, :date, :heure)', array(':date'=>$date, ':heure'=>$heure));
+	public static function addLesson($matiere, $date, $heure , $professeur, $salle, $type_cours)
+	{    
 		$id_date = self::query('SELECT ID FROM dates_de_cours WHERE DATE_C = :date AND HEURE = :heure', array(':date'=>$date, ':heure'=>$heure));
-		self::query('INSERT INTO cours VALUES (:matiere, :id_date, :professeur, :salle)', array(':matiere'=>$matiere, ':id_date'=>$id_date[0], ':professeur'=>$professeur, ':salle'=>$salle));
+		if(!isset($id_date))
+			self::query('INSERT INTO dates_de_cours VALUES (NULL, :date, :heure)', array(':date'=>$date, ':heure'=>$heure));
+		self::query('INSERT INTO cours VALUES (:ID_MATIERE, :ID_DATE, :ID_SALLE, :ID_VACATAIRE, :ID_TYPE_DE_COURS)', array(':ID_MATIERE'=>$matiere, ':ID_DATE'=>$id_date[0], ':ID_SALLE'=>$salle,':ID_VACATAIRE'=>$professeur,':ID_TYPE_DE_COURS'=>$type_cours));
     }
 
 }
