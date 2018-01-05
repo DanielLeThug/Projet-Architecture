8c1791506c29a1e2193cc0fa08bd527eb755d367
Chackal <link684@msn.com>
2018-01-05 16:47:21 +0100

    Edit user marche
    Fonctionnel

--- Views/EditUser_V.php
+++ Views/EditUser_V.php
@@ -2,11 +2,16 @@
 	<?php 
 		$user = EditUser_M::getUser($_GET['id']);
 	?>
-        <form class="form-horizontal" action="add_user" method="post">
+        <form class="form-horizontal" action="edit_user&id=<?php echo $user[0][0] ?>" method="post">
+			<div class="form-group hidden">
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="id" value="<?php if (isset($user[0][0])) echo $user[0][0];?>" name="id">
+                </div>
+            </div>
             <div class="form-group">
                 <label class="control-label col-sm-2" for="nom">Nom :</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="nom" value=<?php if (isset($user[0][2])) echo $user[0][2];?> name="nom">
+                    <input type="text" class="form-control" id="nom" value="<?php if (isset($user[0][2])) echo $user[0][2];?>" name="nom">
                 </div>
             </div>
             <div class="form-group">
@@ -20,18 +25,6 @@
                 <div class="col-sm-10">
                     <input type="email" class="form-control" id="email" value="<?php if (isset($user[0][4])) echo $user[0][4];?>" name="email">
                 </div>
-            </div>
-            <div class="form-group">
-				<label class="control-label col-sm-2" for="utilisateur">Type d'utilisateur :</label>
-				<div class="col-sm-10">
-					<select name="profil" id="profil" class="form-control" size="1">
-					<option value="2">Contrôle de gestion
-					<option value="3">Responsable de Formation
-					<option value="4">Responsable Administratif de formation
-					<option value="5">Responsable Financier
-					<option value="6">Vacataire
-					</select>
-				</div>
             </div>
             <div class="form-group">
                 <div class="col-sm-offset-2 col-sm-10">
