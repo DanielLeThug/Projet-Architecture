d6c4756cd463d90fb4d78c51faf65f7b12475734
Kazouke <kazouke@hotmail.fr>
2018-01-06 18:17:43 +0100

    Jolie petite maison

--- Views/Bandeau_V.php
+++ Views/Bandeau_V.php
@@ -3,4 +3,4 @@
 			  <div class="container-fluid">
 				<a class="navbar-brand" href="index.php">Gestion vacataires</a> 
 				<ul class="nav navbar-nav"> 
-				  <li><a href="index.php">Accueil</a></li> 
+				  <li><a href="index.php"><span class='glyphicon glyphicon-home'></span> Accueil</a></li> 
