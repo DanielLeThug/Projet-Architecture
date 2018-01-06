f66691b01fcbd90c653fd9665833a3fac22a21bf
Unknown <goldennoob68@gmail.com>
2018-01-06 18:24:55 +0100

    view lessons

--- Controllers/ViewLesson_C.php
+++ Controllers/ViewLesson_C.php
@@ -4,7 +4,13 @@ class ViewLesson_C extends Controller {
 
 	public static function doSomething() {
         $userid = self::isLoggedIn();
-		$GLOBALS['lessons'] = ViewLesson_M::listLesson();
+		$profil = Model::getProfil($userid)[0]['profil'];
+		if ($profil == 6) {
+			$GLOBALS['lessons'] = ViewLesson_M::listMyLesson($userid);
+		} else {
+			$GLOBALS['lessons'] = ViewLesson_M::listLesson();
+			echo "wesh";
+		}
 		if(isset($_GET['id']))
 		{
 			$user = EditLesson_M::deleteLesson($_GET['id']);
--- Models/ViewLesson_M.php
+++ Models/ViewLesson_M.php
@@ -3,19 +3,11 @@
 class ViewLesson_M extends Model {
 
 	public static function listLesson() {
-		return self::query("SELECT cours.ID As Id, matieres.LIBELLÉ AS Intitulé,
-							types_de_cours.LIBELLÉ As Type,
-							cours.EFFECTUE As Virement,
-							cours.ID As id_cours,
-							CONCAT(dates_de_cours.DATE_C, ' ' ,dates_de_cours.HEURE) As Date,
-							CONCAT(utilisateurs.PRENOM, ' ' ,utilisateurs.NOM) As Professeur,
-							CONCAT(salles.LIBELLÉ, ' Bât ', batiments.LIBELLÉ) As Salle FROM cours
-							LEFT JOIN matieres ON matieres.ID = cours.ID_MATIERE
-							LEFT JOIN types_de_cours ON types_de_cours.ID = cours.ID_TYPE_DE_COURS
-							LEFT JOIN dates_de_cours ON dates_de_cours.ID = cours.ID_DATE
-							LEFT JOIN salles ON salles.ID = cours.ID_SALLE
-							LEFT JOIN utilisateurs ON utilisateurs.ID = cours.ID_VACATAIRE
-							LEFT JOIN batiments ON batiments.ID = salles.ID_BATIMENT");
+		return self::query("SELECT cours.ID As Id, matieres.LIBELLÉ AS Intitulé, types_de_cours.LIBELLÉ As Type, cours.EFFECTUE As Virement, cours.ID As id_cours, CONCAT(dates_de_cours.DATE_C, ' ' ,dates_de_cours.HEURE) As Date, CONCAT(utilisateurs.PRENOM, ' ' ,utilisateurs.NOM) As Professeur, CONCAT(salles.LIBELLÉ, ' Bât ', batiments.LIBELLÉ) As Salle FROM cours LEFT JOIN matieres ON matieres.ID = cours.ID_MATIERE LEFT JOIN types_de_cours ON types_de_cours.ID = cours.ID_TYPE_DE_COURS LEFT JOIN dates_de_cours ON dates_de_cours.ID = cours.ID_DATE LEFT JOIN salles ON salles.ID = cours.ID_SALLE LEFT JOIN utilisateurs ON utilisateurs.ID = cours.ID_VACATAIRE LEFT JOIN batiments ON batiments.ID = salles.ID_BATIMENT");
+	}
+
+	public static function listMyLesson($userid) {
+		return self::query("SELECT cours.ID As Id, matieres.LIBELLÉ AS Intitulé, types_de_cours.LIBELLÉ As Type, cours.EFFECTUE As Virement, cours.ID As id_cours, CONCAT(dates_de_cours.DATE_C, ' ' ,dates_de_cours.HEURE) As Date, CONCAT(utilisateurs.PRENOM, ' ' ,utilisateurs.NOM) As Professeur, CONCAT(salles.LIBELLÉ, ' Bât ', batiments.LIBELLÉ) As Salle FROM cours LEFT JOIN matieres ON matieres.ID = cours.ID_MATIERE LEFT JOIN types_de_cours ON types_de_cours.ID = cours.ID_TYPE_DE_COURS LEFT JOIN dates_de_cours ON dates_de_cours.ID = cours.ID_DATE LEFT JOIN salles ON salles.ID = cours.ID_SALLE LEFT JOIN utilisateurs ON utilisateurs.ID = cours.ID_VACATAIRE LEFT JOIN batiments ON batiments.ID = salles.ID_BATIMENT WHERE utilisateurs.ID=:ID", array(':ID'=>$userid));
 	}
 
 }
