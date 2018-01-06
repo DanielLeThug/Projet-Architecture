b9c1adafb77645c0632ca8acc7b55fb79354724d
Kazouke <kazouke@hotmail.fr>
2018-01-06 13:30:28 +0100

    Bandeau de référence

--- Views/Bandeau2C_V.php
+++ Views/Bandeau2C_V.php
@@ -1,23 +1,4 @@
-				
-				<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><span class='glyphicon glyphicon-book'></span> Menu des cours<span class="caret"></span></a> 
-					<ul class="dropdown-menu"> 
-						<li><a href="add_lesson"><span class='glyphicon glyphicon-plus'></span> Ajouter un cours</a></li> 
-						<li><a href="view_lesson"><span class='glyphicon glyphicon-th-list'></span> Voir les cours</a></li> 
-					</ul> 
-				</li>
-				<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><span class='glyphicon glyphicon-user'></span> Menu des utilisateurs<span class="caret"></span></a> 
-					<ul class="dropdown-menu"> 
-						<li><a href="add_user"><span class='glyphicon glyphicon-plus'></span> Ajouter un utilisateur</a></li> 
-						<li><a href="view_user"><span class='glyphicon glyphicon-th-list'></span> Voir les utilisateurs</a></li> 
-					</ul> 
-				</li>
-				<li><a href="upload"><span class='glyphicon glyphicon-file'></span> Documents administratifs</a></li> 
-				</ul> 
-				<ul class="nav navbar-nav navbar-right"> 
-				<li> <a href='logout'><span class='glyphicon glyphicon-log-out text-danger'></span> Se déconnecter</a> </li>
-				</ul>
-			</div>
-		  <!-- Navbar content -->
+
 		</nav>
 		<div class="panel panel-primary"> 
 			<div class=panel-heading>
